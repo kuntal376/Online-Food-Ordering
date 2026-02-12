f9b7c8b46b8dc31ebd6848a4020642df4fcc998e
dr-02 <deeproy2001ckt@gmail.com>
2026-02-12 20:45:13 +0530

    VSMenu

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/Header";
+import FoodMenu from "./components/FoodMenu";
 function App(){
     return(
       <>
         <Header />
+        <FoodMenu/>
       </>
     )
 }
---
+++ foodapp/src/components/FoodMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FoodMenu(){
+    return(
+      <>
+        <h1>Hello</h1>
+      </>
+    )
+}
+
+export default FoodMenu
